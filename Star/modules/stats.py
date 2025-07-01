4e19c571eb24eb8ed496bded73d074e1d00069ab
sᴛᴀʀ ʙᴏɪ <bgmim2890@gmail.com>
2025-07-01 23:09:52 +0530

    Add files via upload

---
+++ Star/modules/stats.py
@@ -0,0 +1,18 @@
+from pyrogram import filters, Client
+from pyrogram.types import Message
+
+from Star import OWNER, StarX
+from Star.database.chats import get_served_chats
+from Star.database.users import get_served_users
+
+
+@StarX.on_message(filters.command("stats") & filters.user(OWNER))
+async def stats(cli: Client, message: Message):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await message.reply_text(
+        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
+
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}"""
+    )
